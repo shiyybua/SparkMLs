717d2f363c5d0a5238bac3eb600b069ff7c5cabb
weiwei cai <shiyybua@gmail.com>
2017-10-16 02:34:08 -0700

    fix dimension bug

--- src/main/scala/data_preprocess/phoneRankProcess.scala
+++ src/main/scala/data_preprocess/phoneRankProcess.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.classification.LogisticRegression
 
 
 /**
@@ -20,11 +21,11 @@ object phoneRankProcess{
     val schemaString = header.trim.split(spliter)
     var labelIndex = _labelIndex
     if(labelIndex < 0)
-      labelIndex = schemaString.length + labelIndex - 1
+      labelIndex = schemaString.length + labelIndex
+    val featureSize = schemaString.length - 1
     val rows = line.filter(_ != header).map{ x=>
       var row = x.trim.split(spliter)
       val label = row(labelIndex).toDouble
-      val featureSize = row.length-1
       val features = row.toBuffer
       features.remove(labelIndex)
       val indices = ListBuffer[Int]()
@@ -36,6 +37,7 @@ object phoneRankProcess{
           indices.append(i)
           values.append(value.toDouble)
         }
+
       }
 //      val DataSetFeatures = new SparseVector(featureSize, indices.toArray, values.toArray)
 //      Row(label, DataSetFeatures)
@@ -52,8 +54,30 @@ object phoneRankProcess{
       .builder().master("local[*]")
       .appName("App")
       .getOrCreate()
-    val df = phoneRankProcess.readCSVToDF(sess, "/home/cai/Desktop/phone_rank.csv", -1, ",")
-    df.show()
+    val training = phoneRankProcess.readCSVToDF(sess, "/home/cai/Desktop/phone_rank.csv", 0, ",").cache()
+    val lr = new LogisticRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+
+    // Fit the model
+    val lrModel = lr.fit(training)
+
+    // Print the coefficients and intercept for logistic regression
+    println(s"Coefficients: ${lrModel.coefficientMatrix} Intercept: ${lrModel.interceptVector}")
+
+    // We can also use the multinomial family for binary classification
+    val mlr = new LogisticRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setFamily("multinomial")
+
+    val mlrModel = mlr.fit(training)
+
+    // Print the coefficients and intercepts for logistic regression with multinomial family
+    println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
+    println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
 
   }
 }
